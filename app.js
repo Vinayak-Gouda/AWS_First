645e174580e319e704e0cb1e4cd31d0bc84272ac
Vinayak-Gouda <vinayakgoudanilkod24@gmail.com>
2025-09-15 10:58:17 +0530

    first commit: add nodejs app

---
+++ app.js
@@ -0,0 +1,30 @@
+import express from "express";
+
+const app = express();
+const PORT = 4000;
+
+app.use(express.json());
+
+const Users = [{ id: "1", firstName: "vinayak", lastName: "Gouda", age: "27" }];
+
+app.post("/createUser", (req, res) => {
+  try {
+    const { id, firstName, lastName, age } = req.body;
+
+    Users.push({ id: id, firstName: firstName, lastName: lastName, age: age });
+    res.json({
+      UserInfo: Users,
+      message: "User Created Successfully...",
+    });
+  } catch (err) {
+    console.log("error...",err)
+    res.json({
+      Error: err,
+      message: "user creation failed....",
+    });
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`server running at ${PORT}`);
+});
