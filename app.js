48478d39389a99ed74b9b13404fc9c0a63604291
Vinayak-Gouda <vinayakgoudanilkod24@gmail.com>
2025-09-18 00:02:20 +0530

    api update.

--- app.js
+++ app.js
@@ -5,13 +5,13 @@ const PORT = 4000;
 
 app.use(express.json());
 
-const Users = [{ id: "1", firstName: "vinayak", lastName: "Gouda", age: "27" }];
+const Users = [{ id: "1", firstName: "vinayak", lastName: "Gouda", age: "27", address:"Honnvar" }];
 
 app.post("/createUser", (req, res) => {
   try {
-    const { id, firstName, lastName, age } = req.body;
+    const { id, firstName, lastName, age, address } = req.body;
 
-    Users.push({ id: id, firstName: firstName, lastName: lastName, age: age });
+    Users.push({ id: id, firstName: firstName, lastName: lastName, age: age, address:address });
     res.json({
       UserInfo: Users,
       message: "User Created Successfully...",
